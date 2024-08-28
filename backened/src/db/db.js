31501e17c4331668d2f92bb68c59f1bbd1f77e60
Shoaib Iqbal <iqbalshoaib425@gmail.com>
2024-08-28 20:32:57 +0530

    env in global

--- backened/src/db/db.js
+++ backened/src/db/db.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose"
 import {DB_NAME} from  "../constant.js"
 
+
 const connectDb = async()=>{
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}${DB_NAME}`);
         console.log(`\n MongoDb connected !! DB HOST: ${connectionInstance.connection.host}`)
+        
     } 
     catch (error) {
         console.log("MongoDb Connection failed !", error);
